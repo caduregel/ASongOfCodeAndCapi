eda89873f170c3ff7ff07e54b4d6036d3db2f278
Liam <liam@nehoray.net>
2025-12-13 15:55:48 +0100

    Shadcn adn react router

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,16 @@
+import { Outlet } from "react-router";
+import PublicNav from "./components/PublicNav";
+
 function App() {
   return (
     <div>
-      <p className="text-2xl">Tailwind test</p>
+      <PublicNav />
+      <main className="flex flex-col w-full items-center justify-center">
+        <div className="h-screen">
+
+        </div>
+        <Outlet />
+      </main>
     </div>
   );
 }
