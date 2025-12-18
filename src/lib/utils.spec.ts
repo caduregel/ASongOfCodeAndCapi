e2f0ab308b6d095c48155cee30f98456d3ef49ba
Liam <liam@nehoray.net>
2025-12-18 13:55:01 +0100

    removed describe block

--- src/lib/utils.spec.ts
+++ src/lib/utils.spec.ts
@@ -1,12 +1,10 @@
-import { describe, test } from "vitest";
+import { test } from "vitest";
 
-describe("getIdFromURL spec", () => {
-  test("should extract ID from URL correctly", () => {
-    const getIdFromUrl = (url: string) => url.split("/").pop();
-    const url = "https://www.anapioficeandfire.com/api/characters/823";
-    const id = getIdFromUrl(url);
-    if (id !== "823") {
-      throw new Error(`Expected ID to be '823', but got '${id}'`);
-    }
-  });
+test("should extract ID from URL correctly", () => {
+  const getIdFromUrl = (url: string) => url.split("/").pop();
+  const url = "https://www.anapioficeandfire.com/api/characters/823";
+  const id = getIdFromUrl(url);
+  if (id !== "823") {
+    throw new Error(`Expected ID to be '823', but got '${id}'`);
+  }
 });
